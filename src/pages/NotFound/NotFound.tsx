9f48a751288f3b5f0eafec678dc3efa47fcc4342
levy002 <levy.ukwi002@gmail.com>
2024-07-17 14:27:00 +0200

    task1-KAN-28 add Notfound page

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import SVGIcon from '../../components/SVGIcon/SVGIcon';
+import { ReactComponent as NotFoundSVG } from '../../assets/images/404.svg';
+import MenuNavLink from '../../components/Sidebar/MenuNavLink';
+import { ReactComponent as HomeSVG } from '../../assets/images/home.svg';
+import './notFound.css';
+
+const NotFound: React.FC = () => {
+  return (
+    <section className="not-found">
+      <h1 className='home__title'>DCCS Tuzla</h1>
+      <section className="not-found__contents">
+ <div className="not-found__icon">
+        <SVGIcon Icon={NotFoundSVG} />
+      </div>
+      <h2 className="not-found__title">Page Not Found</h2>
+      <p className="not-found__description">Sorry, the page you requested could not be found. Try navigating back home.</p>
+      <MenuNavLink
+        to='/'
+        desc={
+          <div className="not-found__nav-link">
+            <SVGIcon Icon={HomeSVG} fill='#fff' />
+            <p>Home</p>
+          </div>
+        }
+      />
+      </section>
+    </section>
+  )
+}
+
+export default NotFound;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,15 +3,17 @@ import Home from '../pages/Home/Home';
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import machineLearningExamples from '../data/Example';
 import Example from '../components/Example/Example';
+import NotFound from '../pages/NotFound/NotFound';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+    errorElement: <NotFound />,
     children: [
       {
         index: true,
-        element: <StartScreen />
+        element: <StartScreen />,
       },
       ...machineLearningExamples.map((example) => ({
         path: `/machineLearning/${example.name}`,
