5c5d5f7d1dc84ff335a465f7c0212dc07cfd8a08
levy002 <levy.ukwi002@gmail.com>
2024-07-17 12:19:32 +0200

    task1-KAN-28 configure routes

--- src/components/welcome.tsx
+++
@@ -1,7 +0,0 @@
-import '../assets/styles/welcome.module.css';
-
-const Welcome: React.FC = () => {
-  return <h1>Welcome!!!</h1>;
-};
-
-export default Welcome;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,28 @@
+import StartScreen from '../pages/StartScreen/Start';
+import Home from '../pages/Home/Home';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import machineLearningExamples from '../data/Example';
+import Example from '../components/Example/Example';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    children: [
+      {
+        index: true,
+        element: <StartScreen />
+      },
+      ...machineLearningExamples.map((example) => ({
+        path: `/machineLearning/${example.name}`,
+        element: <Example name={example.name} />,
+      })),
+    ]
+  }
+]);
+
 const Routes: React.FC = () => {
-  return <div>routes</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default Routes;
