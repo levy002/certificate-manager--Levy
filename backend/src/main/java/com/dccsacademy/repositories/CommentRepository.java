2e65be584a3d8d1d05d5d1ad882bb6055d209a23
levy002 <levy.ukwi002@gmail.com>
2024-09-26 21:36:36 +0200

    Implement mappers and repository

---
+++ backend/src/main/java/com/dccsacademy/repositories/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.dccsacademy.repositories;
+
+import com.dccsacademy.entities.CommentEntity;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class CommentRepository implements PanacheRepository<CommentEntity> {
+}
---
+++ backend/src/main/java/com/dccsacademy/utils/mappers/CommentMapper.java
@@ -0,0 +1,36 @@
+package com.dccsacademy.utils.mappers;
+
+import com.dccsacademy.dtos.CommentDto;
+import com.dccsacademy.entities.CommentEntity;
+import com.dccsacademy.entities.CertificateEntity;
+import com.dccsacademy.entities.UserEntity;
+
+public class CommentMapper {
+    public static CommentEntity toEntity(CommentDto dto, CertificateEntity certificate, UserEntity user) {
+        if (dto == null) {
+            return null;
+        }
+
+        CommentEntity commentEntity = new CommentEntity();
+        commentEntity.setId(dto.getId());
+        commentEntity.setComment(dto.getComment());
+        commentEntity.setCertificate(certificate);
+        commentEntity.setUser(user);
+
+        return commentEntity;
+    }
+
+    public static CommentDto toDto(CommentEntity entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(entity.getId());
+        commentDto.setComment(entity.getComment());
+        commentDto.setUserId(entity.getUser().getId());
+        commentDto.setCertificateId(entity.getCertificate().getId());
+
+        return commentDto;
+    }
+}
